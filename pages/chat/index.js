01b1c29f2ac56e56d8988dc58d022d4b901131dd
altrawan <muhammadalifputrasetiawan@gmail.com>
2022-06-27 11:45:50 +0700

    Do formatting fixed

--- pages/chat/index.js
+++ pages/chat/index.js
@@ -1,94 +1,96 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import jwtDecode from 'jwt-decode';
-import io from 'socket.io-client';
-import { useDispatch, useSelector } from 'react-redux';
-import Swal from 'sweetalert2';
-import moment from 'moment';
+import React, { useState } from 'react';
+// import axios from 'axios';
+// import jwtDecode from 'jwt-decode';
+// import io from 'socket.io-client';
+// import { useDispatch, useSelector } from 'react-redux';
+// import Swal from 'sweetalert2';
+// import moment from 'moment';
 import InputEmoji from 'react-input-emoji';
 import styles from 'styles/Chat.module.css';
 import { User, EmailCampaign, IconSend } from 'assets';
 import { Header, Image } from 'components';
-import { API_URL } from 'helpers/env';
-import { wrapper } from 'store/store';
-import {
-  GET_DETAIL_WORKER_SUCCESS,
-  GET_DETAIL_WORKER_FAILED,
-  GET_DETAIL_RECRUITER_SUCCESS,
-  GET_DETAIL_RECRUITER_FAILED,
-} from 'store/types';
+// import { API_URL } from 'helpers/env';
+// import { wrapper } from 'store/store';
+// import {
+//   GET_DETAIL_WORKER_SUCCESS,
+//   GET_DETAIL_WORKER_FAILED,
+//   GET_DETAIL_RECRUITER_SUCCESS,
+//   GET_DETAIL_RECRUITER_FAILED,
+// } from 'store/types';
 
-export const getServerSideProps = wrapper.getServerSideProps(
-  (store) => async (context) => {
-    try {
-      const decoded = jwtDecode(context.req.cookies.token);
+// export const getServerSideProps = wrapper.getServerSideProps(
+//   (store) => async (context) => {
+//     try {
+//       const decoded = jwtDecode(context.req.cookies.token);
 
-      if (decoded.role === 0) {
-        const response = await axios.get(
-          `${API_URL}worker/${decoded.user_id}`,
-          {
-            headers: {
-              Authorization: `Bearer ${context.req.cookies.token}`,
-            },
-          }
-        );
+//       if (decoded.role === 0) {
+//         const response = await axios.get(
+//           `${API_URL}worker/${decoded.user_id}`,
+//           {
+//             headers: {
+//               Authorization: `Bearer ${context.req.cookies.token}`,
+//             },
+//           }
+//         );
 
-        store.dispatch({
-          type: GET_DETAIL_WORKER_SUCCESS,
-          payload: response.data,
-        });
-      } else {
-        const response = await axios.get(
-          `${API_URL}recruiter/${decoded.user_id}`,
-          {
-            headers: {
-              Authorization: `Bearer ${context.req.cookies.token}`,
-            },
-          }
-        );
-        store.dispatch({
-          type: GET_DETAIL_RECRUITER_SUCCESS,
-          payload: response.data,
-        });
-      }
-    } catch (error) {
-      const decoded = jwtDecode(context.req.cookies.token);
+//         store.dispatch({
+//           type: GET_DETAIL_WORKER_SUCCESS,
+//           payload: response.data,
+//         });
+//       } else {
+//         const response = await axios.get(
+//           `${API_URL}recruiter/${decoded.user_id}`,
+//           {
+//             headers: {
+//               Authorization: `Bearer ${context.req.cookies.token}`,
+//             },
+//           }
+//         );
+//         store.dispatch({
+//           type: GET_DETAIL_RECRUITER_SUCCESS,
+//           payload: response.data,
+//         });
+//       }
+//     } catch (error) {
+//       const decoded = jwtDecode(context.req.cookies.token);
 
-      if (decoded.role === 0) {
-        store.dispatch({
-          type: GET_DETAIL_WORKER_FAILED,
-          payload: error.message,
-        });
-      } else {
-        store.dispatch({
-          type: GET_DETAIL_RECRUITER_FAILED,
-          payload: error.message,
-        });
-      }
-    }
+//       if (decoded.role === 0) {
+//         store.dispatch({
+//           type: GET_DETAIL_WORKER_FAILED,
+//           payload: error.message,
+//         });
+//       } else {
+//         store.dispatch({
+//           type: GET_DETAIL_RECRUITER_FAILED,
+//           payload: error.message,
+//         });
+//       }
+//     }
 
-    return {
-      props: {
-        token: context.req.cookies.token,
-      },
-    };
-  }
-);
+//     return {
+//       props: {
+//         token: context.req.cookies.token,
+//       },
+//     };
+//   }
+// );
 
-const index = ({ token }) => {
-  const dispatch = useDispatch();
-  const { listUser, detailUser, detailReceiver } = useSelector(
-    (state) => state
-  );
-  const [socketio, setSocketio] = useState(null);
+const index = () => {
+  // const dispatch = useDispatch();
+  // const { listUser, detailUser, detailReceiver } = useSelector(
+  //   (state) => state
+  // );
+  // const [socketio, setSocketio] = useState(null);
   const [message, setMessage] = useState('');
-  const [listChat, setListChat] = useState([]);
-  const [editChat, setEditChat] = useState('');
-  const [editMessage, setEditMessage] = useState(null);
+  // const [listChat, setListChat] = useState([]);
+  // const [editChat, setEditChat] = useState('');
+  // const [editMessage, setEditMessage] = useState(null);
 
-  const [empty, setEmpty] = useState(true);
+  // const [empty, setEmpty] = useState(true);
 
-  useEffect(() => {}, []);
+  // useEffect(() => {}, []);
+
+  const empty = true;
 
   return (
     <>
